04d423d73c13311659b9e817c38ba33ce09a9d3a
Bryan Deagle <bryan.deagle@ni.com>
2020-07-14 13:16:58 -0500

    13

--- 13 Fibonacci.py
+++ 13 Fibonacci.py
@@ -6,3 +6,15 @@ sequence to generate.(Hint: The Fibonnaci seqence is a sequence of numbers
 where the next number in the sequence is the sum of the previous two numbers
 in the sequence. The sequence looks like this: 1, 1, 2, 3, 5, 8, 13, …)
 """
+
+
+fibs = [1, 1]
+amount = int(input('How many fibonacci numbers:'))
+if amount == 1:
+    print([1])
+elif amount == 2:
+    print(fibs)
+else:
+    for i in range(amount-2):
+        fibs.append(fibs[-1] + fibs[-2])
+    print(fibs)
